81e223516bce834e8d1c88d05c41e3e6b3ad1132
Kitiara Rivera <kitiarasr@gmail.com>
2017-09-26 13:07:54 -0700

    finish2

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -20,22 +20,31 @@ namespace TechJobs.Controllers
         public IActionResult Results(string searchType, string searchTerm) //under search i should find something based on category and also based on all.
         {
             ViewBag.columns = ListController.columnChoices;
-            if (searchType.Equals("all"))
+            if (searchType.Equals("all") && string.IsNullOrEmpty(searchTerm))
             {
                 ViewBag.jobs = JobData.FindAll();
                 ViewBag.title = "All Jobs";
                 return View("Index");
             }
 
-            else
+            else if(searchType.Equals("all"))
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+                ViewBag.title = "All Values";
+                return View("Index");
+
+
+            }
+            else if (!string.IsNullOrEmpty(searchTerm))
             {
                 ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);  //if im finding by a criteria, it should show things based on that
                 ViewBag.column = searchType;
                 ViewBag.title = "All " +  ListController.columnChoices[searchType] + " Values";
                 return View("Index");
             }
+            return View("Index");
 
-            
+
         }
 
     }
